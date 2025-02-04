e6a8cbbe7915b8a88409b485b2c8e6ba533b4611
manasupes <srivastavamanasfe@gmail.com>
2025-02-04 08:12:19 +0530

    Configured daisy UI, added a Nav bar with Product name.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,37 @@ function App() {
 
   return (
     <>
-      <h1>Hello World!!</h1>
+    <div className="navbar bg-primary">
+    <div className="flex-1">
+      <a className="btn btn-ghost text-xl bg-slate-50">
+        <img className="h-7" src="../favicon.png"></img>
+        <span>DevProfileFYI</span>
+      </a>
+    </div>
+    <div className="flex-none gap-2">
+      <div className="dropdown dropdown-end">
+        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS Navbar component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+          </div>
+        </div>
+        <ul
+          tabIndex={0}
+          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+          <li>
+            <a className="justify-between">
+              Profile
+              <span className="badge">New</span>
+            </a>
+          </li>
+          <li><a>Settings</a></li>
+          <li><a>Logout</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
     </>
   )
 }
